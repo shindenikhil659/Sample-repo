33641acee392d554ff83cb61e5a885f2a9e37234
Nikhil Shinde <shindenikhil659@gmail.com>
2024-03-29 16:57:16 +0530

    Create Sample.java

---
+++ Sample.java
@@ -0,0 +1,7 @@
+public class Sample {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+	System.out.println("HOW Are Your Buddy??");
+	System.out.println("I am Fine...");
+    }
+}
